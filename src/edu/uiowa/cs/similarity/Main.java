e5cad18310aa60e67240a944ac09656d74fa4953
Ryan <ryan-price@uiowa.edu>
2018-04-24 17:52:43 -0500

    Almost finished part one

--- src/edu/uiowa/cs/similarity/Main.java
+++ src/edu/uiowa/cs/similarity/Main.java
@@ -19,43 +19,48 @@ public class Main {
         System.out.println("quit - Quit this program");
         System.out.println("index - Name of the file. Creates the index of a file");
     }
+    private static ArrayList getStopWords() throws FileNotFoundException{
+        ArrayList<String> stopwords = new ArrayList();
+        String stopWords = "../stopwords.txt";
+        Scanner sw = new Scanner(new File(stopWords));
+        while (sw.hasNext()) {
+            stopwords.add(sw.next());
+        }
+        return stopwords;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String delimiters = "[.?!\\s]";
-
+        String delimiters = "[.?!,\\s]";
+        LinkedList<ArrayList> sentences = new LinkedList();
+        ArrayList stopwords=getStopWords();
         while (true) {
             System.out.print("> ");
             String command = input.readLine();
             if (command.equals("help") || command.equals("h")) {
                 printMenu();
-            } else if (command.equals("quit")) {
+            } 
+            else if (command.equals("quit")) {
                 System.exit(0);
-            } else if (command.equals("index")) {
+            } 
+            else if (command.contains("index")) {
                 ArrayList<String> oneSentence = new ArrayList();
-                LinkedList<ArrayList> allSentences = new LinkedList();
-                LinkedList<ArrayList> sentences = new LinkedList();
-                ArrayList<String> stopwords = new ArrayList();
-                Stack<ArrayList> stack = new Stack();
-                System.out.println("file name");
-                String fileName = input.readLine();
-                String stopWords = "../stopwords.txt";
-                //file name   ../cleanup_test.txt
-                Scanner sc = new Scanner(new File(fileName));//.useDelimiter(delimiters);
-                //Scanner oneSentence;
-                Scanner sw = new Scanner(new File(stopWords));
-                PorterStemmer porterStemmer = new PorterStemmer();
-                //ArrayList oneSentence;
-                String word;
-                ArrayList temp = new ArrayList();
-                while (sw.hasNext()) {
-                    stopwords.add(sw.next());
+                ArrayList temp;
+                String fileName; //  ../cleanup_test.txt
+                if(command.length()<6){
+                    System.out.println("Enter file name");
+                    fileName = input.readLine();
                 }
-                for (int i = 0; sc.hasNext(); i++) {
-                    int j = 0;
-                    //words.add(porterStemmer.stem(sc.nextLine().toLowerCase()));
-                    //word = sc.next();
-                    //while (!sc.next().equals("?") && !sc.next().equals(".") && !sc.next().equals("!")) {
+                else{
+                    fileName=command.substring(6);
+                }
+                System.out.println("Indexing "+fileName);
+                Scanner sc = new Scanner(new File(fileName));
+                PorterStemmer porterStemmer = new PorterStemmer();
+                String word;
+
+                while(sc.hasNext()){   
+
                     word = porterStemmer.stem(sc.next().toLowerCase());
                     while (true) {
                         if (!word.contains("?") && !word.contains(".") && !word.contains("!") && sc.hasNext()) {
@@ -63,36 +68,28 @@ public class Main {
                                 oneSentence.add(word);
                             }
                             word = porterStemmer.stem(sc.next().toLowerCase());
-                        } else {
+                        } 
+                        else {
                             oneSentence.add(word.substring(0, word.length() - 1));
                             break;
                         }
-                        j++;
-                        //sc.next();
-                    }
-                    stack.push(oneSentence);
-                    //allSentences.push(oneSentence);
-                    //sentences.add(stack.pop());
-                    allSentences.add(stack.pop());
-                    sentences = allSentences;
-                    //sentences.push(stack.pop());
-                   oneSentence.clear();
-                   //allSentences.clear();
-                    
-                }
-                //oneSentence = words;
-                //words.add(word);
-//                    if (!stopwords.contains(oneSentence.get(i))) {
-//                        //sentences.add(porterStemmer.stem(word));
-//                        sentences.push(words);
-//                        //sentences.push(porterStemmer.stem(word));
-//                    }
-                //}
 
-                System.out.println("Num sentences");
+                    }
+                    temp=new ArrayList(oneSentence);
+                    sentences.add(temp);
+                    oneSentence.clear();
+                }
+
+            } 
+            else if(command.equals("sentences")){
                 System.out.println(sentences);
+                System.out.println("Num sentences");
                 System.out.println(sentences.size());
-            } else {
+            }
+            else if(command.equals("vectors")){
+                
+            }
+            else {
                 System.err.println("Unrecognized command");
             }
         }
