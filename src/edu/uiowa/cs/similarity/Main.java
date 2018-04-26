b989ebe74021c94923c87d8a99e6e568a3e91b5d
Ryan <ryan-price@uiowa.edu>
2018-04-26 14:50:51 -0500

    Worked on vector and indexing

--- src/edu/uiowa/cs/similarity/Main.java
+++ src/edu/uiowa/cs/similarity/Main.java
@@ -1,18 +1,14 @@
 package edu.uiowa.cs.similarity;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.AbstractSet;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.Stack;
 import opennlp.tools.stemmer.*;
+
 
 public class Main {
 
@@ -23,8 +19,8 @@ public class Main {
         System.out.println("index - Name of the file. Creates the index of a file");
     }
 
-    private static ArrayList getStopWords() throws FileNotFoundException {
-        ArrayList<String> stopwords = new ArrayList();
+    private static Set getStopWords() throws FileNotFoundException {
+        Set<String> stopwords = new HashSet();
         String stopWords = "../stopwords.txt";
         Scanner sw = new Scanner(new File(stopWords));
         while (sw.hasNext()) {
@@ -41,16 +37,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         LinkedList<ArrayList> sentences = new LinkedList();
-        ArrayList stopwords = getStopWords();
+        Set<String> stopwords = getStopWords();
+        String delimiter = "[?!.]";
         //Vectors vector;
+        Vector aVector = null;
         while (true) {
             System.out.print("> ");
             String command = input.readLine();
             if (command.equals("help") || command.equals("h")) {
                 printMenu();
-            } else if (command.equals("quit")) {
+            } 
+            else if (command.equals("quit")) {
                 System.exit(0);
-            } else if (command.contains("index")) {
+            } 
+            else if (command.contains("index")) {
                 ArrayList<String> oneSentence = new ArrayList();
                 ArrayList temp;
                 String fileName; //  ../cleanup_test.txt
@@ -58,106 +58,89 @@ public class Main {
                     System.out.println("Enter file name");
                     fileName = input.readLine();
                 } else {
-                    fileName = command.substring(6);
+                    fileName=command.substring(6);// six being the index after the space after index
                 }
                 System.out.println("Indexing " + fileName);
-                Scanner sc = new Scanner(new File(fileName));
+                Scanner sc = new Scanner(new File(fileName)).useDelimiter(delimiter);
                 PorterStemmer porterStemmer = new PorterStemmer();
                 String word;
-
+                String sentence;
                 while (sc.hasNext()) {
-                    word = sc.next().toLowerCase();
-                    while (true) {
-                        if (!word.contains("?") && !word.contains(".") && !word.contains("!") && sc.hasNext()) {
-                            if (!stopwords.contains(word)) {
-                                word = porterStemmer.stem(word);
-                                if (word.contains(",")) {
-                                    oneSentence.add(word.substring(0, word.length() - 1));
-                                } else {
-                                    oneSentence.add(word);
-                                }
-                            }
-                            word = sc.next().toLowerCase();
-                        } else {
-                            word = word.substring(0, word.length() - 1);
-                            if (word.contains("?") || word.contains(".") || word.contains("!")) {
-                                word = word.substring(0, word.length() - 1);
-                            }
-                            word = porterStemmer.stem(word);
-                            oneSentence.add(word);
-                            break;
+                    //read in a sentence
+                    sentence = sc.next().toLowerCase();
+                    Scanner sw = new Scanner(sentence);
+                    while(sw.hasNext()){
+                        //should have individual words now
+                        word=sw.next().toLowerCase();
+                        
+                        if(!stopwords.contains(word) && word.length()>1){
+                           word = porterStemmer.stem(word);
+                           if(word.contains(",")){
+                               oneSentence.add(word.substring(0,word.length()-1));
+                           }
+                           else{
+                                oneSentence.add(word);
+                           }
                         }
                     }
-                    temp = new ArrayList(oneSentence);
-                    sentences.add(temp);
-                    oneSentence.clear();
+                    if(!oneSentence.isEmpty()){
+                        temp= new ArrayList(oneSentence);
+                        aVector.addToMap(oneSentence);
+                        sentences.add(temp);
+                        oneSentence.clear();
+                    }
+                    
                 }
-                 Vectors vector =new Vectors(sentences);
-            } else if (command.equals("sentences")) {
+                //aVector = new Vector(sentences);
+            }
+//                    word = sc.next().toLowerCase();
+//                    while (true) {
+//                        if (!word.contains("?") && !word.contains(".") && !word.contains("!") && sc.hasNext()) {
+//                            if (!stopwords.contains(word)) {
+//                                word = porterStemmer.stem(word);
+//                                if (word.contains(",")) {
+//                                    oneSentence.add(word.substring(0, word.length() - 1));
+//                                } else {
+//                                    oneSentence.add(word);
+//                                }
+//                            }
+//                            word = sc.next().toLowerCase();
+//                        } else {
+//                            word = word.substring(0, word.length() - 1);
+//                            if (word.contains("?") || word.contains(".") || word.contains("!")) {
+//                                word = word.substring(0, word.length() - 1);
+//                            }
+//                            word = porterStemmer.stem(word);
+//                            oneSentence.add(word);
+//                            break;
+//                        }
+//                    }
+//                    temp = new ArrayList(oneSentence);
+//                    sentences.add(temp);
+//                    oneSentence.clear();
+//                }
+//                 aVector = new Vector(sentences);
+//                }
+//            }
+            else if (command.equals("sentences")) {
                 System.out.println(sentences);
                 System.out.println("Number of sentences");
                 System.out.println(sentences.size());
-            } else if (command.equals("vectors")) {
-                    
-            } else {
+            }
+            else if (command.equals("vectors")) {
+                if(aVector == null){
+                    System.out.println("Please index a file");
+                }
+                else{
+                    aVector.print();
+                }
+            }
+            else {
                 System.err.println("Unrecognized command");
             }
         }
     }
-    
-    private static class Vectors{
-        //public Map<String, Integer> subMap;
-        public Map<String, Map<String, Double>> vectorMap;
-        
-      
-        private Vectors(LinkedList<ArrayList> sentences) {
-            this.vectorMap=new HashMap();
-            ArrayList sentence; 
-            String word; // key for vector map
-            Map<String, Double> subMap = new HashMap();
-            Set<String> listOfWords = new HashSet<>();
-            Map<String, Map<String, Double>> vectorMap= new HashMap();
-            for (int i=0;i<sentences.size();i++){ //Fills the map that will go within the vectorMap
-                sentence=sentences.get(i);
-                for(int j=0; j<sentence.size();j++){
-                    word= (String)sentence.get(j);
-                    subMap.put(word, 0.0); 
-                    
-                }
-            }
-            System.out.println("SubMap of words " +subMap);
-            //Fills the vectorMap
-            for (int k=0;k<sentences.size();k++){ //Fills the map that will go within the vectorMap
-                sentence=sentences.get(k);
-                for(int m=0; m<sentence.size();m++){
-                    word = (String)sentence.get(m);
-                    vectorMap.put(word, subMap); 
-                    
-                }
-            }
-            String keyWord;
-            String subKeyWord;
-            double value;
-            this.vectorMap=vectorMap;
-            System.out.println("VectorMap of words " +vectorMap);
-            //Add the values for words
-            for(int i = 0; i<sentences.size();i++){
-                sentence=sentences.get(i);
-                for(int j=0; j<sentence.size();j++){
-                    keyWord=(String)sentence.get(j);
-                    subMap=this.vectorMap.get(keyWord);
-                    for(int k=0; k<sentence.size();k++){
-                        subKeyWord=(String)sentence.get(k);
-                        value=subMap.get(subKeyWord);
-                        subMap.replace(subKeyWord, value, value+1);
-                    }
-                    this.vectorMap.put(keyWord, subMap);
-                }
-                
-            }
-            System.out.println("New vectorMap "+this.vectorMap);
-       }
-    }
+
 }
     
 
