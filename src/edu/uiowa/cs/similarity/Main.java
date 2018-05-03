3ec46b3b40dfb15a8d1f2811347c7b8c3c174f7c
Adam Sheagren <adamms0521@gmail.com>
2018-05-03 12:59:58 -0500

    started eucnorm

--- src/edu/uiowa/cs/similarity/Main.java
+++ src/edu/uiowa/cs/similarity/Main.java
@@ -19,6 +19,8 @@ public class Main {
         System.out.println("sentences - Prints the sentences and number of sentences");
         System.out.println("vectors - Prints the vectors");
         System.out.println("topj- Input: Word(Q) and number(J). Find the J most simalar words to Q");
+        System.out.println("within topj: euc: Word(Q) and number(J). Find the euclidean distance of two vectors.");
+        System.out.println("within topj: eucnorm: Word(Q) and number(J). Find the normal euclidean distance of two vectors");
     }
 
     private static Set getStopWords() throws FileNotFoundException {
@@ -116,7 +118,7 @@ public class Main {
                 if(choiceWord.equals("euc")){
                     aVector.computeEucDistance(word, number);
                 } else if(choiceWord.equals("eucnorm")){
-                    
+                    aVector.computeNormEuc(word, number);
                 } else if(choiceWord.equals("cosine")) {
                     aVector.computeTopJ(word, number);
                 } else{
